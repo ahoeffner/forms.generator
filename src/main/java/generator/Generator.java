9306c1d8130df19301f4082cbc62d8310f91f781
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2023-01-12 12:28:18 +0100

    .

---
+++ src/main/java/generator/Generator.java
@@ -0,0 +1,15 @@
+package generator;
+
+public class Generator
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello");
+    Generator generator = new Generator();
+  }
+
+
+  public Generator()
+  {
+  }
+}
