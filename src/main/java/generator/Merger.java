d3e55d9bd7456dfd97d98ace6de19c1d34cbb48c
ahoeffner <alex@hoeffner.net>
2023-12-01 20:52:52 +0100

    Update Merger.java

--- src/main/java/generator/Merger.java
+++ src/main/java/generator/Merger.java
@@ -22,45 +22,49 @@ public class Merger
 	private Node merge(Element section)
 	{
 		boolean done = false;
-		Element node = section.clone();
 
 		while (!done)
 		{
 			done = true;
-			Elements elements = node.getAllElements();
+			Elements elements = section.getAllElements();
 
 			for (int i = 0; i < elements.size(); i++)
 			{
 				Element elem = elements.get(i);
-
-				if (elem.tagName().equals(Generator.COLUMN))
-				{
-					column(elem);
-					done = false;
-				}
-
-				else
-
-				if (elem.attributes().hasKey(Generator.COLUMNS))
-				{
-					elem.attributes().remove(Generator.COLUMNS);
-					columns(elem);
-					done = false;
-				}
-
-				if (!done)
-					break;
+				done = replace(elem);
+				if (!done) break;
 			}
 		}
 
-		return(node);
+		return(section);
+	}
 
+
+	private boolean replace(Element elem)
+	{
+		if (elem.tagName().equals(Generator.COLUMN))
+		{
+			column(elem);
+			return(false);
+		}
+
+		else
+
+		if (elem.attributes().hasKey(Generator.COLUMNS))
+		{
+			elem.attributes().remove(Generator.COLUMNS);
+			columns(elem);
+			return(false);
+		}
+
+		return(true);
 	}
 
 
 	private void column(Element elem)
 	{
-		System.out.println(elem+" "+this.column);
+		Node field = template.fieldnodes.get(column);
+		this.replace(elem,field);
 	}
 
 
@@ -80,29 +84,21 @@ public class Merger
 
 			Element replace = template.clone();
 			this.template.replace(replace,colattrs);
-			System.out.println(replace);
 			merged.appendChild(replace);
 			this.merge(replace);
 			this.column = null;
 		}
 
-		if (merged.childNodes().size() == 0)
-		{
-			replace(elem,merged);
-		}
-		else
-		{
-			Element next = elem;
-			ArrayList<Node> children = new ArrayList<Node>(merged.childNodes());
+		Element next = elem;
+		ArrayList<Node> children = new ArrayList<Node>(merged.childNodes());
 
-			for (int i = 0; i < children.size(); i++)
-			{
-				next.after(children.get(i));
-				next = (Element) children.get(i);
-			}
-
-			delete(elem);
+		for (int i = 0; i < children.size(); i++)
+		{
+			next.after(children.get(i));
+			next = (Element) children.get(i);
 		}
+
+		delete(elem);
 	}
 
 
